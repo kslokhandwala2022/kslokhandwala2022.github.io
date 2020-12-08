e7d71a036cfebe089371fcda4c471c0643c5863c
Kaamil Lokhandwala <kslokhandwala@wpi.edu>
2020-12-08 18:11:08 -0500

    testing timer for groupmusic

--- InDevelopment/GroupMusic/index.js
+++ InDevelopment/GroupMusic/index.js
@@ -1,86 +1,117 @@
 const db = firebase.firestore();
-
-let memberID;
-let memberName;
-let memberslist;
-let bandName;
-let isHost = false;
-let instruments = [
+const READYING_UP = 0;
+const PLAYING_MUSIC = 1;
+const DONE_PLAYING = 2;
+const instruments = [
 	'conductor',
-	'flute gal',
-	'guitar man',
-	'trumpet guy',
-	'bassist'
+	'melody',
+	'bass',
+	'chord'
 ];
 
-async function setID() {
-	let snapshot = await db.doc('Members/allMembers').get();
-	memberID = snapshot.data().Count;
-	console.log(`This is the member ID: ${memberID}`);
-	memberName = localStorage.getItem('user');
 
-	document.getElementById('info').innerHTML = `Member ID: ${memberID}` + '<br>' + `Member Name: ${memberName}`;
-	document.getElementById('instrument').innerHTML = `You are the ${instruments[memberID % instruments.length]
-		}`;
+let memberName;
+let memberDoc;
+let memberRole;//reset on song start
+
+let bandName;
+let bandDoc;
+let bandState;//reset on song start
+let leaveBand;
+
+let isHost = false;
+
+
+//#region HELPER FUNCTIONS
+
+/**
+ * starts Timer for song for set number of minutes
+ * @param {string} renderTarget 
+ */
+function startTimer(renderTarget) {
+	const minutes = 1
+	const currentTime = Date.parse(new Date());
+	const deadline = new Date(currentTime + minutes * 60 * 1000);
+	initializeClock(renderTarget, deadline);
 }
 
-function displayMembers() {
-	//document.getElementById('allMembers').innerHTML = `Member ID: ${memberID}`;
-	db.doc(`Bands/${bandName}`).onSnapshot(function(doc) {
-		console.log("SnapShot activated");
-		db.collection(`Bands/${bandName}/members`).get().then( function(members){
-			let memberslist = members.docs.map(doc => doc.data());
-			//console.log(`Global MembersList: ${memberslist}`);
-			document.getElementById("allMembers").innerHTML = "Your Band: " + bandName + "<br>";
-			
-			memberslist.forEach(function (item, index) {
-				document.getElementById("allMembers").innerHTML += index + ":" + item.userName + "<br>";
-				//console.log(index, ' => ', item.userName);
-			})
+/**
+ * calculates time to target endtime
+ * @param {number} endtime 
+ */
+function getTimeRemaining(endtime) {
+	const total = Date.parse(endtime) - Date.parse(new Date());
+	const seconds = Math.floor((total / 1000) % 60);
+	const minutes = Math.floor((total / 1000 / 60) % 60);
+	const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+	const days = Math.floor(total / (1000 * 60 * 60 * 24));
 
-			db.doc(`Bands/${bandName}`).set({//if memberslist is diff from current, snapshot will not rerun
-				members: memberslist.length,
-			})
-		})
-	})
-	
-	// let snapshot = await db.collection('Bands').doc(bandName).collection('members').get();
-	// memberslist = snapshot.docs.map(doc => doc.data());
-	// console.log(`Global MembersList: ${memberslist}`);
-	// //console.log(`Global MembersList: ${memberslist[8]}`);
-	// document.getElementById("allMembers").innerHTML = "Your Band: " + bandName + "<br>";
-	// memberslist.forEach(function (item, index) {
-	// 	// doc.data() is never undefined for query doc snapshots
-	// 	document.getElementById("allMembers").innerHTML += index + ":" + item.userName + "<br>";
-	// 	console.log(index, ' => ', item.userName);
-	// })
+	return {
+		total,
+		days,
+		hours,
+		minutes,
+		seconds
+	};
 }
 
-async function getDoc(path) {
-	return await db.doc(path).get();
+/**
+ * sets a clock div to count down
+ * @param {string} id 
+ * @param {number} endtime 
+ */
+function initializeClock(id, endtime) {//to use for backend stuff set id = false, take in function for end timer event
+	const clock = document.getElementById(id);
+	function updateClock() {
+		const t = getTimeRemaining(endtime);
+		clock.style.display = 'block';
+		clock.innerHTML = 'days: ' + t.days + '<br>' +
+			'hours: ' + t.hours + '<br>' +
+			'minutes: ' + t.minutes + '<br>' +
+			'seconds: ' + t.seconds;
+		if (t.total <= 0) {
+			clearInterval(timeinterval);
+			if(isHost){
+				bandDoc.update({
+					status : DONE_PLAYING
+				})
+			}
+		}
+	}
+
+	updateClock(); // run function once at first to avoid delay
+	let timeinterval = setInterval(updateClock, 1000);
 }
 
+//#endregion
+
+
+
+//#region FINDING A BAND
+
+/**
+ * empty at the moment
+ * runs when join page starts
+ */
 function onPageLoaded() {
+
+	//localStorage.clear();
 	//add to count
-	db.doc('Members/allMembers')
-		.get()
-		.then(querySnap => {
-			console.log(`${querySnap.id} => ${querySnap.data().Count}`);
+	// db.doc('Members/allMembers')
+	// 	.get()
+	// 	.then(querySnap => {
+	// 		console.log(`${querySnap.id} => ${querySnap.data().Count}`);
 
-			db.doc('Members/allMembers').update({
-				Count: querySnap.data().Count + 1
-			});
-		});
+	// 		db.doc('Members/allMembers').update({
+	// 			Count: querySnap.data().Count + 1
+	// 		});
+	// 	});
 }
 
-function onRoomLoaded() {
-	bandName = localStorage.getItem('band');
-	setID();
-	displayMembers();
-}
-
-
-//unhide forms and close buttons
+/**
+ * unhide forms and close button
+ * @param {boolean} hostBool 
+ */
 function openForm(hostBool) {
 	isHost = hostBool;
 	document.getElementById('createRoom').style.display = 'none';
@@ -90,75 +121,251 @@ function openForm(hostBool) {
 	let formTitle = document.getElementById('joinChoice');
 	if (isHost) {
 		formTitle.style.display = 'block';
-		formTitle.innerHTML = 'Create a Band';
+		formTitle.textContent = 'Create a Band';
 	} else {
 		formTitle.style.display = 'block';
-		formTitle.innerHTML = 'Join a Band';
+		formTitle.textContent = 'Join a Band';
 	}
 }
 
-//take data and start room or error
+/**
+ * Attempts to create a new band or join existing one
+ * Sets local storage for user and band to prep for page change
+ * Will not create if band name exists as another
+ * Will not join if band has more than 10 members, or if they are in the middle of playing
+ */
 function enterRoom() {
-	
+
 	bandName = document.getElementById('band').value;
 	let userName = document.getElementById('name').value;
 	let passCode = document.getElementById('passcode').value;
 	localStorage.setItem('user', userName);
 	localStorage.setItem('band', bandName);
-	//console.log(`BandName: ${bandName}`);
+	localStorage.setItem('isHost', isHost);
 
-	// if(!isHost){
-	//     let n = getDoc(`Bands/${bandName}`)
-	//     let realCode = n.data().passCode;
-	//     if(passCode != realCode){
-	//         document.getElementById("errorRoom").style.display = "block";
-	//         return;
-	//     }
-	// }
+	const joinRef = db.collection('Bands').doc(bandName);
 
-
-	//make band
-	db.collection('Bands')
-		.doc(bandName)
-		.set({
-			bandname: bandName,
-			members: 0,
-			status: 'readying up',
-			passCode: passCode
-		})
-		.then(() =>
-			//now make user a member of the band
-			db
-				.collection('Bands')
-				.doc(bandName)
-				.collection('members')
-				.doc(userName)
-				.set({
-					userName: userName
-				})
-		).then(() =>
-			location.href = 'room.html'
-		)
-		
-	//setID();
-	//displayMembers();
-	// document.getElementById('bandInfo').style.display = 'none';
-	
-	//document.getElementById('mainRoom').style.display = 'block';
-
-}
-
-function onPageLeaving() {
-	//subtract from count
-	db.collection('Members')
-		.doc('allMembers')
-		.get()
-		.then(querySnapshot => {
-			db.doc('Members/allMembers').update({
-				Count: querySnapshot.data().Count - 1
+	if (isHost) {
+		joinRef.get()
+			.then((docSnapshot) => {
+				if (docSnapshot.exists) {//Error
+					const errorRoom = document.getElementById('errorRoom');
+					errorRoom.style.display = 'block';
+					errorRoom.textContent = 'This band name is taken';
+				} else {
+					joinRef.set({
+						bandname: bandName,
+						members: 0,
+						status: READYING_UP,
+						passCode: passCode,
+						createdAt: new Date().getTime(),
+						host: userName
+					})
+						.then(() =>
+							//now make user a member of the band
+							joinRef.collection('members').doc(userName).set({
+								userName: userName,
+								role: 'none',
+							})
+						).then(() =>
+							location.href = 'room.html'
+						)
+				}
 			});
+	}
+	else if (!isHost) {
+		joinRef.get().then((docSnapshot) => {
+			if (docSnapshot.exists) {
+				//are they in the middle of a song?
+				//are they full?
+				//now make user a member of the band
+				if (docSnapshot.data().members > 50) {
+					const errorRoom = document.getElementById('errorRoom');
+					errorRoom.style.display = 'block';
+					errorRoom.textContent = 'This band has too many members - try another one';
+				}
+				else if (docSnapshot.data().status != READYING_UP) {
+					const errorRoom = document.getElementById('errorRoom');
+					errorRoom.style.display = 'block';
+					errorRoom.textContent = 'The band is in the middle of playing! Wait until the room is open.';
+				}
+				else {
+
+					joinRef.collection('members').doc(userName).set({
+						userName: userName,
+						role: 'none'
+					})
+						.then(() =>
+							location.href = 'room.html'
+						)
+				}
+
+			} else {
+				const errorRoom = document.getElementById('errorRoom');
+				errorRoom.style.display = 'block';
+				errorRoom.textContent = 'The band you are looking for does not exist!';
+			}
 		});
+	}
+
+
+	/**
+	 * if HOST
+	 * check if band name already exists
+	 * if exists, error, with 'Name Taken'
+	 * else make band, add user
+	 * 
+	 * if JOINING
+	 * check if band exists
+	 * if exists, add member to band, redirect
+	 * else error with 'band exists'
+	 */
+
+
+
+	//make band if host
+
 }
+
+//#endregion
+
+//#region PLAYING A SONG
+
+
+/**
+ * Runs as soon as member joins music room
+ * Sets all global variables, except member role and bandstate
+ */
+function onJoinRoom() {
+	console.log('joining room');
+	memberName = localStorage.getItem('user');
+	bandName = localStorage.getItem('band');
+	isHost = localStorage.getItem('isHost');
+
+	memberDoc = db.doc(`Bands/${bandName}/members/${memberName}`);
+	bandDoc = db.doc(`Bands/${bandName}`);
+
+	document.getElementById('info').textContent = `Member Name: ${memberName}`;
+	document.getElementById('instrument').textContent = `You are the ${instruments[90 % instruments.length]}`;
+	if(isHost){
+		document.getElementById('hostStart').style.display = '';
+		console.log('should show button');
+	}
+	else if(!isHost){
+		document.getElementById('hostStart').style.display = 'none';
+		console.log('dont show button');
+
+	}
+	getBandInfo();
+}
+
+/**
+ * Runs right after member joins room
+ * Sets a band document listener to listen for band state
+ * Continually updates number of members present
+ */
+function getBandInfo() {
+	leaveBand = bandDoc.onSnapshot(function (doc) {
+		console.log("SnapShot activated");
+
+		//Band State changes
+		const oldState = bandState;
+		bandState = doc.data().status;
+		if(oldState != bandState){
+			console.log('Detected band state change: ' + oldState + ' to --> ' + bandState);
+			bandStateChange(bandState);
+		}
+
+		bandDoc.collection('members').get().then(function (members) {
+			let memberslist = members.docs.map(doc => doc.data());
+			//console.log(`MembersList: ${memberslist}`);
+			document.getElementById("allMembers").innerHTML = "Your Band: " + bandName + "<br>";
+
+			memberslist.forEach(function (item, index) {
+				document.getElementById("allMembers").innerHTML += index + ":" + item.userName + "<br>";
+				//console.log(index, ' => ', item.userName);
+			})
+
+			bandDoc.update({//if memberslist is diff from current, snapshot will not rerun
+				members: memberslist.length,
+			})
+		})
+	})
+}
+
+/**
+ * Changes UI of scene based on band state
+ */
+function bandStateChange(state){
+	switch(state){
+		case READYING_UP:
+			//what UI elements are we hiding
+			//which ones, if any are revealed?
+			break;
+		case PLAYING_MUSIC:
+			//start Timer
+			startTimer('clockdiv');
+			break;
+		case DONE_PLAYING:
+			//hide music UI
+			break;
+		default:
+			;
+	}
+}
+
+/**
+ * Based on role and boolean show or hide music UI
+ * @param {string} role 
+ */
+function toggleMusicUI(role, show){
+	switch(role){
+		case 'conductor':
+			break;
+		default:
+	}
+}
+
+/**
+ * Host-only function
+ * Updates band status to start everyone's timers
+ * Triggers member assignment logic
+ */
+function beginSong() {
+	//set band status to playing
+	bandDoc.update({
+		status: PLAYING_MUSIC
+	})
+	//assign member roles
+	document.getElementById('hostStart').style.display = 'none';
+
+}
+
+/**
+ * Runs as soon as timer ends
+ * Shows buttons for play again or quit
+ */
+function endSong() {
+
+}
+//#endregion
+
+
+
+
+
+
+// function onPageLeaving() {
+// 	//subtract from count
+// 	db.collection('Members')
+// 		.doc('allMembers')
+// 		.get()
+// 		.then(querySnapshot => {
+// 			db.doc('Members/allMembers').update({
+// 				Count: querySnapshot.data().Count - 1
+// 			});
+// 		});
+// }
 
 // db.collection("users").doc("frank").update({
 //     "age": 13,
@@ -226,4 +433,3 @@ function onPageLeaving() {
 //     querySnapshot.forEach((doc) => {
 //         console.log(`${doc.id} => ${doc.data()}`);
 //     });
-// });
